298f7053db37c694447e3b887efd4e86e3137362
Serg-Klimenko <klimenko.sm.job@gmail.com>
2020-08-27 19:26:09 +0300

    add create Docker container (action-ui.test.ts)

--- src/__tests__/action-ui.test.ts
+++ src/__tests__/action-ui.test.ts
@@ -2,7 +2,10 @@ import playwright from "playwright";
 
 describe ("UI test action", () => {
     test("Google search", async () => {
-        const browser = await playwright.firefox.launch({headless: false});
+        //
+//        const browser = await playwright.firefox.launch({headless: false});
+        // Запуск браузера для Docker
+        const browser = await playwright.firefox.launch({headless: true, args: ['--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage']});
         const context = await browser.newContext();
         const page = await context.newPage();
         await page.goto("https://www.google.com");
